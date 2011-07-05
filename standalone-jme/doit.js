f604e1e1edcb1d8a6fad501c03e4d76ac95d6c02
Jonathan Fine <jf3243@pclt1384-linux.(none)>
2011-07-05 17:45:55 +0100

    Add test that '2^(3 + 4)' gives '2^{ 3 + 4 }', which passes.

--- standalone-jme/doit.js
+++ standalone-jme/doit.js
@@ -9,3 +9,11 @@ load('runtime/scripts/math.js');
 load('runtime/scripts/util.js');
 load('runtime/scripts/jme.js');
 load('runtime/scripts/jme-display.js');
+
+var actual = Numbas.jme.display.exprToLaTeX('2^(3+4)');
+var expect = '2^{ 3 + 4 }';
+
+if (actual === expect)
+    print('ok');
+else
+    print('error');
