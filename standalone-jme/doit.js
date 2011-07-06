6a26a80394582b4714f0e36cc4b428e4cd219d41
Jonathan Fine <jf3243@pclt1384-linux.(none)>
2011-07-06 10:00:52 +0100

    Create and use expr-to-latex.js, a concatenation of source files.

--- standalone-jme/doit.js
+++ standalone-jme/doit.js
@@ -5,10 +5,7 @@ Numbas.queueScript = function(file, deps, callback){
     callback();
     };
 
-load('runtime/scripts/math.js');
-load('runtime/scripts/util.js');
-load('runtime/scripts/jme.js');
-load('runtime/scripts/jme-display.js');
+load('local/expr-to-latex.js');
 
 var actual = Numbas.jme.display.exprToLaTeX('2^(3+4)');
 var expect = '2^{ 3 + 4 }';
--- standalone-jme/doit.sh
+++ standalone-jme/doit.sh
@@ -1,1 +1,8 @@
+cat \
+    runtime/scripts/math.js \
+    runtime/scripts/util.js \
+    runtime/scripts/jme.js \
+    runtime/scripts/jme-display.js \
+    > local/expr-to-latex.js
+
 js standalone-jme/doit.js
