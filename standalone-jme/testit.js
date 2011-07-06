3a8e88acfc068a9d5b8874cb271c6a200e5939ac
Jonathan Fine <J.Fine@open.ac.uk>
2011-07-06 12:16:07 +0100

    In testit.js, loop throught the test data.

--- standalone-jme/testit.js
+++ standalone-jme/testit.js
@@ -1,3 +1,18 @@
+
+// See: https://developer.mozilla.org/en/Rhino_Shell
+var test_data = eval(readFile('local/test-data.json'));
+
+var i = 0;
+var n = test_data.length;
+var item;
+
+for (i=0; i < n; i++){
+    
+    item = test_data[i];
+    print(item);
+}
+
+
 var actual = Numbas.jme.display.exprToLaTeX('2^(3+4)');
 var expect = '2^{ 3 + 4 }';
 
