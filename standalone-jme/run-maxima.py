f48a4b2a8cbc668ce670996f7996f9f8e009fefc
Jonathan Fine <J.Fine@open.ac.uk>
2011-08-11 16:59:31 +0100

    Revise run-maxima.py to produce list of (input, raw_output) pairs.

--- standalone-jme/run-maxima.py
+++ standalone-jme/run-maxima.py
@@ -4,36 +4,60 @@ Generate maxima output using new file run-maxima.py.
 
 import subprocess
 
-def doit():
-    
-    tgt_filename = 'local/examples.maxima'
-
-    lines = open('standalone-jme/examples.txt', 'rb')
-    tmp = open(tgt_filename, 'wb')
+def filter_input(lines):
 
     for line in lines:
         line = line.strip()
-        if not line:
+        if not line or line[0] == '#':
+            continue
+        yield line
+
+
+def filter_output(lines):
+
+    for line in lines:
+        if line.startswith('(%i'):
+
+            # This is a bit yucky and obscure.
+            bits = line.split(None, 1)
+            if len(bits) == 1:
+                yield ''
+            else:
+                yield bits[1]
+        else:
             continue
 
-        if line[0] == '#':
-            continue
 
-        tmp.write('tex(%s);\n' % line)
-    tmp.close()
+def doit():
+    
+    # Create the input stream.
+    tgt_filename = 'local/examples.maxima'
+    lines = open('standalone-jme/examples.txt', 'rb')
+    
+    examples = list(filter_input(lines))
+    pending = [
+        'tex(%s);\n' % example
+        for example in examples
+        ]
+    number_of_examples = len(pending)    
+    pending.append('quit();\n')
+    stdin = ''.join(pending)
 
-    args = ['maxima', '-b', tgt_filename]
-    args = ['maxima', 'batch-b', tgt_filename]
     args = ['maxima']
-
     p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
-    stdout, stderr = p.communicate(open(tgt_filename).read() + 'quit();\n')
+    stdout, stderr = p.communicate(stdin)
     p.wait()
 
-    print stdout
 
+    output_lines = stdout.split('\n')
+    example_output = list(filter_output(output_lines))
+
+    a, b = len(pending), len(example_output)
+    assert a == b, ('length', a, b)
+
+    return zip(examples, example_output)
 
 if __name__ == '__main__':
 
-    doit()
+    x = doit()
